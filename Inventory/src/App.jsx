f3d80baee9de1414c0597154adf02a9eb3221d4b
jeevant010 <jeevantmudgil10@gmail.com>
2025-10-13 22:18:20 +0530

    Frontend is setup from here

---
+++ Inventory/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+    </>
+  )
+}
+
+export default App
