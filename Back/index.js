b3676443f3d838a00930615b621ddf67d3e612a8
jeevant010 <jeevantmudgil10@gmail.com>
2025-10-14 13:46:48 +0530

    this is server updated

--- Back/index.js
+++ Back/index.js
@@ -35,7 +35,7 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(compression());
-app.use(express.json({ limit: '2mb' }));
+// app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ extended: true }));
 // app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
 
