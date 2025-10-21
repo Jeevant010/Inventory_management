15dfa1d4701a35c8e6863192db942eb24e0de082
jeevant010 <jeevantmudgil10@gmail.com>
2025-10-21 20:26:56 +0530

    allow origin in cors for frontend

--- Back/index.js
+++ Back/index.js
@@ -28,7 +28,7 @@ app.use(sanitizeEmptyStrings); // sanitize before routes
 app.use(cookieParser());
 
 app.use(cors({
-    origin: [ "http://localhost:5173", "http://localhost:5174" ],
+    origin: [ "http://localhost:5173", "http://localhost:5174" , "https://inventory-management-lovat-seven.vercel.app" ],
     methods: "GET,POST,PUT,DELETE,PATCH",
     credentials: true
 }));
